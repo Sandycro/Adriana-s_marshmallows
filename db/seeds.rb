5b4b271a8a2b94a1d6514976bda067a0134c01b1
sandy smircic <sandy.smircic@hotmail.com>
2020-07-18 14:41:18 +0100

    Seeded categories, stylesheet for catgory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+Category.destroy_all
+
+puts "Creating products"
+
+Category.create(name: "Birthdays")
+Category.create(name: "Weddings")
+Category.create(name: "Hens")
+Category.create(name: "Mother's Day")
+Category.create(name: "Halloween")
+Category.create(name: "Christmas")
+Category.create(name: "St Patrick's Day")
+Category.create(name: "Pride")
+Category.create(name: "Easter")
+Category.create(name: "Valentines")
+
+puts "Finished!"
