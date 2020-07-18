72cd75377d5a547eb619c15b450fa06fbf8533ef
sandy smircic <sandy.smircic@hotmail.com>
2020-07-18 12:15:14 +0100

    Created models Category, Product, Picture and Review

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
-module.exports = environment
+# const { environment } = require('@rails/webpacker')
+# const webpack = require('webpack')
+# environment.plugins.prepend('Provide',
+#   new webpack.ProvidePlugin({
+#     $: 'jquery',
+#     jQuery: 'jquery',
+#     Popper: ['popper.js', 'default']
+#   })
+# )
+# module.exports = environment
---
+++ db/migrate/20200718111057_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.integer :price
+      t.integer :rating
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
