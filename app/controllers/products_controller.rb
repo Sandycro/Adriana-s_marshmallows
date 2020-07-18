1e638c753af2f662cb6bfaa484c119d302e1170a
sandy smircic <sandy.smircic@hotmail.com>
2020-07-18 14:23:43 +0100

    Navbar added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+  def new
+    @category = Category.find(params[:category_id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    # we need `category_id` to associate product with corresponding category
+    @category = Category.find(params[:category_id])
+    @product.category = @category
+    if @product.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
 end
