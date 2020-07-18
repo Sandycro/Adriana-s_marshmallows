30e5d0a6a8c41a0e843001173ccfed8634df686e
sandy smircic <sandy.smircic@hotmail.com>
2020-07-18 10:06:57 +0100

    Bootstrap imported

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+module.exports = environment
